39038f8ddca058ac6620184507d499d4e4fb0516
Fernando Lopes <32465227+fernandojuniorlopes@users.noreply.github.com>
2019-10-29 11:01:26 +0000

    Created Author Model and Added an Authors controller (scaffolding)

---
+++ Books/Data/BooksDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Books.Models
+{
+    public class BooksDbContext : DbContext
+    {
+        public BooksDbContext (DbContextOptions<BooksDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Books.Models.Author> Author { get; set; }
+    }
+}
--- Books/Startup.cs
+++ Books/Startup.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Books.Models;
 
 namespace Books
 {
@@ -24,6 +26,9 @@ namespace Books
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<BooksDbContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("BooksDbContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
